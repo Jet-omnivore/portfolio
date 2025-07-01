ca323a3f6945e946af9beffb542168fb1e64c799
Jet-omnivore <jetomnivore@gmail.com>
2025-07-02 02:24:22 +0530

    added nextPageAnimation on HeroPage

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+export default function AboutMe() {
+  
+
+ return (
+   <section id="aboutme" className="w-full h-full">
+      
+   </section>
+ );
+}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { SplitText } from "gsap/all";
+import { Observer, SplitText } from "gsap/all";
+import CustomEase from 'gsap/CustomEase';
 
 function Icon() {
   return (
@@ -13,10 +14,40 @@ function Icon() {
 export default function Hero() {
 
   useGSAP(() => {
+    
+    CustomEase.create("easeInBack", "0.36, 0, 0.66, -0.56");
+
     const split = SplitText.create(".split-char", {
       type: "chars",
       charsClass: "char++"
     });
+
+    const nextPageTl = gsap.timeline({duration: 0.5});
+    nextPageTl.to('.username', {
+      x: "150vw",
+      ease: "easeInBack",
+      })
+    .to('body', {
+      backgroundSize: 0,
+      ease: "easeInBack"
+    }, "<0.2")
+    .to('.subtitle', {
+      x: "-50vw",
+      ease: "easeInBack",
+    }, "<-0.2")
+    .pause()
+     
+
+    Observer.create(
+    {
+      type: "wheel",
+      target: "hero",
+      onWheel: (self) => {
+        if (self.deltaY > 0) nextPageTl.play()
+        else nextPageTl.reverse();
+      }
+    });
+
 
     gsap.from(split.chars, {
       autoAlpha: 0.5,
@@ -31,15 +62,15 @@ export default function Hero() {
 
   return (
     <section id="hero" className="w-full h-full relative overflow-hidden">
-      <nav className="flex justify-end py-4 px-12">
+      <nav className="flex justify-end py-4 px-12 navbar">
         <Icon /> 
         <Icon /> 
         <Icon /> 
       </nav>
       <div className="absolute bottom-[0] w-full flex justify-center">
         <div>
-        <h3 className="mb-[2em] text-text-secondary split-lines">I build games, apps, websites, and graphics.</h3>
-        <h1 className="mix-blend-difference text-[min(12vw,230px)] leading-[0.684] text-text-accent split-char"> <strong>JET OMNIVORE</strong> </h1>
+        <h3 className="mb-[2em] text-text-secondary split-lines subtitle">I build games, apps, websites, and graphics.</h3>
+        <h1 className="mix-blend-difference text-[min(12vw,230px)] leading-[0.684] text-text-accent split-char username"> <strong>JET OMNIVORE</strong> </h1>
         </div>
       </div>
     
